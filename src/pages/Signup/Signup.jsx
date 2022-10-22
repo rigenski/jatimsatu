ddc81b1f2bf7990a7d653e33a5d55856153433c0
Rigen Maulana <rygen.dev@gmail.com>
2022-10-22 23:52:23 +0700

    fix: change form input register

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -7,112 +7,301 @@ const Signup = () => {
   return (
     <>
       <main>
-        <section className="auth bg-primary-2">
+        <section className="auth register bg-primary-2">
           <div className="container h-100">
             <div className="auth-wrapper h-100 py-5 d-flex flex-column justify-content-center align-items-center">
               <h1 className="mb-4 text-heading-1 text-white">Jatimsatu</h1>
               <div className="card">
-                <div className="card-body px-3 py-5 p-md-5">
+                <div className="register card-body px-3 py-5 p-md-5">
                   <form action="">
                     <div className="mb-4">
                       <h3 className="mb-3 text-heading-3 text-grey-1 text-center">
-                        Daftar
+                        Register
                       </h3>
                       <p className="text-body-2 text-grey-3 text-center">
-                        Daftarkan akun anda dengan mengisi formulir berikut.
-                        Anda sudah punya akun?{" "}
-                        <a
-                          href="/signin"
-                          className="text-primary-2 fw-semibold"
-                        >
-                          Masuk disini
-                        </a>
+                        Silahkan melengkapi data pribadi Anda agar data Anda
+                        dapat diverifikasi oleh admin kami
                       </p>
                     </div>
                     <div className="mb-4">
-                      <div className="mb-3">
-                        <label
-                          htmlFor="name"
-                          className="form-label text-body-3 text-grey-1"
-                        >
-                          Nama
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="name"
-                          placeholder="Masukkan nama anda disini..."
-                        />
+                      <div className="row">
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="nama"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Nama <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="nama"
+                              placeholder="Masukkan nama lengkap Anda"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="no-hp"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              No. HP <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="no-hp"
+                              placeholder="Masukkan nomor hp"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="password"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Password <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="password"
+                              placeholder="Masukkan password"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="password"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Konfirmasi Password{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="password"
+                              placeholder="Masukkan ulang password"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="nik"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              NIK <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="nik"
+                              placeholder="Masukkan nomor induk keluarga"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="upload-kk"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Upload KK <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              id="upload-kk"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="upload-ktp"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Upload KTP <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              id="upload-ktp"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="tempat-lahir"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Tempat Lahir{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="tempat-lahir"
+                              placeholder="Masukkan tempat lahir sesuai KTP"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="tanggal-lahir"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Tanggal Lahir{" "}
+                              <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="date"
+                              className="form-control"
+                              id="tanggal-lahir"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="alamat"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Alamat <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="alamat"
+                              placeholder="Masukkan alamat sesuai KTP"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="desa"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Desa <span className="text-danger">*</span>
+                            </label>
+                            <select class="form-select" id="desa">
+                              <option value="">---pilih salah satu---</option>
+                              <option value="1">Blitar</option>
+                              <option value="2">Malang</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="kecamatan"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Kecamatan <span className="text-danger">*</span>
+                            </label>
+                            <select class="form-select" id="kecamatan">
+                              <option value="">---pilih salah satu---</option>
+                              <option value="1">Blitar</option>
+                              <option value="2">Malang</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="kelurahan"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Kelurahan <span className="text-danger">*</span>
+                            </label>
+                            <select class="form-select" id="kelurahan">
+                              <option value="">---pilih salah satu---</option>
+                              <option value="1">Blitar</option>
+                              <option value="2">Malang</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3 mb-md-0">
+                            <label
+                              htmlFor="kode-pos"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Kode Pos <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="kode-pos"
+                              placeholder="Masukkan kode pos "
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="rt"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              RT <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="rt"
+                              placeholder="Masukkan RT sesuai KTP"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="rw"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              RW <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="rw"
+                              placeholder="Masukkan RW sesuai KTP"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <div>
+                            <label
+                              htmlFor="pekerjaan"
+                              className="form-label text-body-3 text-grey-1"
+                            >
+                              Pekerjaan <span className="text-danger">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="pekerjaan"
+                              placeholder="Masukkan pekerjaan anda"
+                            />
+                          </div>
+                        </div>
                       </div>
-                      <div className="mb-3">
-                        <label
-                          htmlFor="phone"
-                          className="form-label text-body-3 text-grey-1"
-                        >
-                          Nomor HP
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="phone"
-                          placeholder="Masukkan nomor hp anda disini..."
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <label
-                          htmlFor="password"
-                          className="form-label text-body-3 text-grey-1"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          className="form-control text-body-4 text-grey-4"
-                          id="password"
-                          placeholder="Masukkan password anda disini..."
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <label
-                          htmlFor="confirm_password"
-                          className="form-label text-body-3 text-grey-1"
-                        >
-                          Konfirmasi Password
-                        </label>
-                        <input
-                          type="password"
-                          className="form-control text-body-4 text-grey-4"
-                          id="confirm_password"
-                          placeholder="Masukkan ulang password baru anda disini..."
-                        />
-                      </div>
-                      <div className="mb-2">
-                        <label
-                          htmlFor="nik"
-                          className="form-label text-body-3 text-grey-1"
-                        >
-                          NIK
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="nik"
-                          placeholder="Masukkan nomor induk keluarga anda disini..."
-                        />
-                      </div>
-                      <p className="text-body-4 text-grey-3 mb-0">
-                        Lupa kata sandi?{" "}
-                        <a href="" className="text-primary-2 fw-semibold">
-                          Klik disini
-                        </a>
-                      </p>
                     </div>
                     <div className="d-flex justify-content-center align-items-center">
                       <button
                         type="button"
-                        className="button btn-lg w-100 text-button text-white bg-primary-2 text-center border-0 rounded"
+                        className="button btn-lg text-button text-white bg-primary-2 text-center border-0 rounded"
                       >
-                        Daftar
+                        Register
                       </button>
                     </div>
                   </form>
